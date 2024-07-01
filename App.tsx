0488c37033a33156cb442d80999a432378e0f533
aligenzzz <alina_bybko@mail.ru>
2024-07-01 18:49:19 +0300

    feat: add android styles (statusbar, navigationbar, keybord, splashscreen)

--- App.tsx
+++ App.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from "react";
-import SplashScreen from "react-native-splash-screen";
+import React from "react";
 import AppNavigator from "./src/components/navigation";
 
 function App(): React.JSX.Element {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     SplashScreen.hide();
-  //   }, 2000);
-  // }, []);
-
   return <AppNavigator />;
 }
 
