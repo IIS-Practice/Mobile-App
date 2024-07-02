44286e4b3ad285777ab97c3f35121c9c9af60909
UladzislauSachyuka <94551428+UladzislauSachyuka@users.noreply.github.com>
2024-07-02 18:05:51 +0300

    feat: finished webDev screen

---
+++ src/svg/CircleIcon.jsx
@@ -0,0 +1,13 @@
+import { Path, Svg, Rect } from 'react-native-svg';
+
+const CircleIconSvg = () => {
+  return (
+    <Svg width="41" height="42" viewBox="0 0 41 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Rect width="41" height="42" rx="20.5" fill="white"/>
+      <Path d="M18.74 29.98V13.02H21.26V29.98H18.74ZM11.3 22.7V20.34H28.7V22.7H11.3Z" fill="#8DB9FC"/>
+    </Svg>
+
+  );
+};
+
+export default CircleIconSvg;
